ba96d77daab4b19e5844a56e665adac2a4a09931
Ahmed <157909403+DevTeto@users.noreply.github.com>
2024-02-24 18:41:00 +0200

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -20,11 +20,11 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://telegra.ph/file/d6a3b43ebcf4326351723.jpg", caption=f"¤¦ لا يمكنك استخدام البوت\n\n¤¦ الا بعد الاشتراك بقناة البوت\n\n¤¦ اشترك بقناة بعدها ارسل /start .",
+                    photo="https://telegra.ph/file/d6a3b43ebcf4326351723.jpg", caption=f"╮⦿ لا يمكنك استخدام البوت\n\n│᚜⦿ الا بعد الاشتراك بقناة البوت\n\n╯⦿ اشترك بقناة بعدها ارسل /start .",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("إضغط للاشتراك بالقناة", url=link),
+                                InlineKeyboardButton("اطغـط هنـا للاشتـراك بالقـناه ✅", url=link),
                             ]
                         ]
                     )
