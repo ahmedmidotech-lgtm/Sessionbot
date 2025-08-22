fa54fb1094b352dc35277ceaf1c2cc5eb5f1d7fe
AhmedMido <ahmedmidotech@gmail.com>
2025-08-22 17:06:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 # تحميل بيانات .env
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))           # لازم يبقى رقم صحيح
-API_HASH = os.getenv("API_HASH")            # نص طويل
-BOT_TOKEN = os.getenv("BOT_TOKEN")          # التوكن بتاع البوت
-OWNER_ID = int(os.getenv("OWNER_ID"))       # رقم ID بتاعك
-MONGO_DB_URI = os.getenv("MONGO_DB_URI")    # لينك قاعدة بيانات مونجو
+API_ID = 23099888
+API_HASH = "794b3646805ec06f48e4451c77420750"
+BOT_TOKEN = "8406728249:AAEpMlVV66zLu-_s-F5RkqkKQaL-G9q7E5k"
+OWNER_ID = 8439899351
+MONGO_DB_URI = "mongodb+srv://ahmedtawfikmido:6a8fgZhHJZzpZ7jR@cluster0.o1wgcs0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
