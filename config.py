9f690023a5af8ea80ea35b8a98d1185a350fddf8
AhmedMido <ahmedmidotech@gmail.com>
2025-08-22 16:55:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-API_ID=23099888
-API_HASH=794b3646805ec06f48e4451c77420750
-BOT_TOKEN=8406728249:AAEpMlVV66zLu-_s-F5RkqkKQaL-G9q7E5k
-OWNER_ID=8439899351
-MONGO_DB_URI=mongodb+srv://ahmedtawfikmido:6a8fgZhHJZzpZ7jR@cluster0.o1wgcs0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))        # رقم
+API_HASH = os.getenv("API_HASH")         # نص
+BOT_TOKEN = os.getenv("BOT_TOKEN")       # توكن البوت
+OWNER_ID = int(os.getenv("OWNER_ID"))    # رقم ID بتاعك
+MONGO_DB_URI = os.getenv("MONGO_DB_URI") # لينك المونجو
